66c0a162cc24a2888251e6c0b39c96ba11c22c37
Varun Pujari <varunpvp@gmail.com>
2019-05-27 09:56:50 +0530

    Updates

---
+++ precache-manifest.189cc6e81d6650d95d8a17c6ff131578.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d485418f8bf39cbec7f98f73831e354b",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "8d8f6132ee8416216058",
+    "url": "/react-demo/static/css/2.536f1d1e.chunk.css"
+  },
+  {
+    "revision": "16091a6d216c9c65d498",
+    "url": "/react-demo/static/css/main.4c151972.chunk.css"
+  },
+  {
+    "revision": "8d8f6132ee8416216058",
+    "url": "/react-demo/static/js/2.e9bf0032.chunk.js"
+  },
+  {
+    "revision": "16091a6d216c9c65d498",
+    "url": "/react-demo/static/js/main.448b8b5d.chunk.js"
+  },
+  {
+    "revision": "78404b4b0f010a1ef451",
+    "url": "/react-demo/static/js/runtime~main.3dcdfef1.js"
+  },
+  {
+    "revision": "85f8716d624e0945de3b5b8fbabd0e86",
+    "url": "/react-demo/static/media/Live-lessons.85f8716d.png"
+  },
+  {
+    "revision": "9c9145204e441948bcc36c40c51e5412",
+    "url": "/react-demo/static/media/Solve-puzzles.9c914520.png"
+  },
+  {
+    "revision": "8c43bac53728d906bb0edb34afe61e0e",
+    "url": "/react-demo/static/media/blindbot.8c43bac5.png"
+  },
+  {
+    "revision": "e10590b9e1ef4176a082e804b57e0a4a",
+    "url": "/react-demo/static/media/hero.e10590b9.png"
+  },
+  {
+    "revision": "f96bfd9bf1efcea4031725038da5b812",
+    "url": "/react-demo/static/media/play-feature.f96bfd9b.png"
+  },
+  {
+    "revision": "716665a10f49afb0845d2468b32ca86f",
+    "url": "/react-demo/static/media/practice-positions.716665a1.png"
+  },
+  {
+    "revision": "2d87ba1d86d02510d5c4286fa3de5711",
+    "url": "/react-demo/static/media/store-games.2d87ba1d.png"
+  }
+]);
